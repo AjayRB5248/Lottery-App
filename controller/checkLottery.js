7134eb96584799fe6a8c47b7f7fb332ea19077b2
Ajay Ranabhat <47922566+arb12345@users.noreply.github.com>
2023-08-10 11:46:05 +0545

    Update checkLottery.js

--- controller/checkLottery.js
+++ controller/checkLottery.js
@@ -58,6 +58,58 @@ const checkMMLottery = async (req, res) => {
   });
 };
 
+const checkPBLottery = async (req, res) => {
+  const userNumbers = req.query.userNumber.trim().split(',').map(Number);
+  console.log({ userNumbers });
+  const winningPowerball = winningNumbers.powerball;
+
+  if (userNumbers.length !== 6 || userNumbers.some(isNaN)) {
+    res.status(400).json({
+      error: 'Invalid input. Please provide 6 numbers separated by commas.',
+    });
+    return;
+  }
+  const matchedNumbersInfo = comparePowerballNumbers(
+    userNumbers.slice(0, 5),
+    userNumbers[5],
+    winningPowerball.numbers
+  );
+
+  const prize = calculatePowerball(
+    matchedNumbersInfo.matchedNumbers,
+    matchedNumbersInfo.hasPowerball,
+    winningPowerball.powerball
+  );
+
+  // Update the user's lottery history
+  try {
+    const userId = req.user.token;
+    await User.findByIdAndUpdate(userId, {
+      $push: {
+        lotteryHistory: {
+          numbers: userNumbers.slice(0, 5),
+          powerball: userNumbers[5],
+          timestamp: new Date(),
+        },
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 'An error occurred while updating lottery history.',
+    });
+  }
+
+  res.json({
+    winningNumbers: winningPowerball.numbers,
+    powerball: winningPowerball.powerball,
+    userNumbers: userNumbers.slice(0, 5),
+    userPowerball: userNumbers[5],
+    prize,
+  });
+};
+
+
+
 module.exports = {
   checkMMLottery,
 };
