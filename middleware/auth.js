5221109e117d50025e73b296570550212eec63da
Ajay Ranabhat <47922566+arb12345@users.noreply.github.com>
2023-08-12 10:50:38 +0545

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,9 @@
 const { verifyToken } = require('../utils/jwtHelper'); // Update with the correct path
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) {
-    return res.sendStatus(401);
-  }
-
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
   const decodedToken = verifyToken(token);
-  if (!decodedToken) {
-    return res.sendStatus(403);
-  }
 
   req.user = decodedToken;
   next();
