3a240aa483e91fbf8298e9c62d36e56d2c1dbabd
arb12345 <ajayranabhat19@gmail.com>
2023-08-15 11:27:33 +0545

    fix userHistory limit

--- controller/getUserHistory.js
+++ controller/getUserHistory.js
@@ -10,10 +10,7 @@ const getUserHistory = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    // Extract the latest 5 entries from the lotteryHistory array
-    const latestLotteryHistory = user.lotteryHistory.slice(-5);
-
-    res.status(200).json({ lotteryHistory: latestLotteryHistory });
+    res.status(200).json({ user });
   } catch (error) {
     res.status(500).json({ message: 'Error fetching user profile' });
   }
