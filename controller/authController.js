4d73444cac6dc8627b8fac78688eac82da60734b
ajayrumsan <ajay.rumsan@gmail.com>
2023-08-23 09:34:48 +0545

    added token while register too

--- controller/authController.js
+++ controller/authController.js
@@ -1,21 +1,24 @@
-const User = require('../models/User');
-const { hashPassword, comparePasswords } = require('../utils/bcryptHelper');
-const { generateToken } = require('../utils/jwtHelper');
+const User = require("../models/User");
+const { hashPassword, comparePasswords } = require("../utils/bcryptHelper");
+const { generateToken } = require("../utils/jwtHelper");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(409).json({ message: 'Email already registered' });
+      return res.status(409).json({ message: "Email already registered" });
     }
 
     const hashedPassword = await hashPassword(password);
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
-    res.status(201).json({ newUser, message: 'User registered successfully' });
+    const token = generateToken({ token: newUser._id });
+    res
+      .status(201)
+      .json({ newUser, token, message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ message: 'Registration failed', error });
+    res.status(500).json({ message: "Registration failed", error });
   }
 };
 
@@ -25,18 +28,18 @@ const login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: "Authentication failed" });
     }
 
     const isMatch = await comparePasswords(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Authentication failed' });
+      return res.status(401).json({ message: "Authentication failed" });
     }
 
     const token = generateToken({ token: user._id });
     res.status(200).json({ user, token });
   } catch (error) {
-    res.status(500).json({ message: 'Authentication failed', error });
+    res.status(500).json({ message: "Authentication failed", error });
   }
 };
 
